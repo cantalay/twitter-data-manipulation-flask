02e307b89ee20d99039a0b60545a168883e3330b
talayankara <can.talay12@hacettepe.edu.tr>
2019-05-03 02:07:17 +0300

    Commit for GAE

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from twython import Twython
 import numpy as np
+from string import digits
 import re
 from collections import Counter,OrderedDict
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -13,54 +14,53 @@ APP_SECRET = "7ihL6R70skJywPDWaFk8lsjIEY6NsYbIZIgkhD6ogdyBFkvjix"
 OAUTH_TOKEN = "1370537623-Brb7RYIklcMKrRcNYbMq1alWZK8WtXy7uaDmLul"
 OAUTH_TOKEN_SECRET = "D4kfNfIcD36vkqa9mp0sy75K61AcOmcXZvdJPh09OFzKZ"
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
 @app.route('/',  methods=['GET', 'POST'])
-def hello_world():
+def twitterApi():
     post = request.form.get('selectData')
     if post == None:
         post = "Zeytin Dalı"
-    print(post)
+    print("Search Keyword",post)
 
     wordnum = []
     allWord = []
     tweet4tfidf = []
 
-
-    query = post + ' AND -filter:retweets AND -filter:replies'
+    x = 0
+    query = post + ' AND -filter:retweets'
     try:
         results = twitter.cursor(twitter.search, q=query)
         for result in results:
-                lowercase = result['text'].lower()
-                print(lowercase)
-                word = word2 = re.sub('http://\S+|https://\S+', '', lowercase)
-                tweet4tfidf.append(word)
-                tweetList = changeTweet(lowercase)
-                wordnum.append(len(tweetList))
-                allWord += tweetList
+            x+=1
+            lowercase = result['text'].lower()
+            word = re.sub('http://\S+|https://\S+', '', lowercase)
+            removedigit = str.maketrans('', '', digits)
+            word = word.translate(removedigit)
+            tweet4tfidf.append(word)
+            tweetList = changeTweet(word)
+            wordnum.append(len(tweetList))
+            allWord += tweetList
 
     except StopIteration as e:
         print(e)
     except:
         print("Other Itteration Error")
-    tfidfAnalyse(tweet4tfidf)
-    for x,y in mostCommon(allWord):
-        print(x,"--",y)
+
+    print("TFIDF TWEET",tweet4tfidf)
+    if not tweet4tfidf == []:
+        tfidfAnalyse(tweet4tfidf)
     frequent = mostCommon(allWord)
-    print("Most Frequently : ", mostCommon(allWord))
-    print("Word num : ", wordnum)
-    print("all Word: ", allWord)
-    print("Average : ", np.mean(wordnum))
-    print(np.mean(wordnum))
 
     return render_template(
         'index.html',
         frequent=frequent,
         average=np.mean(wordnum),
         significant=tfidfAnalyse(tweet4tfidf),
+        countedTweet = x,
         post=post)
 
 
 def changeTweet(word):
+
     word = word.replace(".", "")
     word = word.replace(",", "")
     word = word.replace("'", "")
@@ -86,8 +86,7 @@ def tfidfAnalyse(tweet):
     tfidf_transformer = TfidfTransformer()
     tfidf_matrix = tfidf_transformer.fit_transform(data)
     word2tfidf = dict(zip(cv.get_feature_names(), tfidf_transformer.idf_))
-    sorteddict = sorted(word2tfidf.items(), key=lambda kv: kv[1], reverse=True)[5:15]
-    print("SORT",sorteddict)
+    sorteddict = sorted(word2tfidf.items(), key=lambda x: x[1], reverse=True)
     return sorteddict
 
 
